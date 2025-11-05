5e8aaf1578c4ba058ed2dcd4f25a50c216b2071f
SergioYago <sergioya@ucm.es>
2025-11-05 22:56:15 +0100

    arreglo un poco

--- skeleton/ExplosionGenerator.cpp
+++ skeleton/ExplosionGenerator.cpp
@@ -1,5 +1,5 @@
 #include "ExplosionGenerator.h"
-
+#include <cmath>
 ExplosionGenerator::ExplosionGenerator(Vector3D pos, float fuerza, float rangeini, float rangend, float tim):GeneradorFuerza({0,0,0},fuerza)
 {
 	mPos = pos;
@@ -14,10 +14,12 @@ Vector3D ExplosionGenerator::getForce(Particle* aux)
 {
 	Vector3D pos = aux->getPos();
 	pos =pos- mPos;
+	Vector3D sol = { 0,0,0 };
 	if(active&&pos.norm()<=rangeAct)
 	{
-	
+		sol = pos * (fuerzAct / (pos.norm() * pos.norm())) * exp(-(timAct/time));	
 	}
+	return sol;
 }
 
 void ExplosionGenerator::update(float t)
@@ -25,8 +27,8 @@ void ExplosionGenerator::update(float t)
 	//cuando timAct == 0 fuerza=0 tengo que ir restando con respecto a timAct
 	if (active) {
 		timAct -= t;
-		if (timAct <= 0) { active = false; }
-
+		if (timAct <= 0) { active = false; fuerzAct = 0; }
+		else { fuerzAct = (fuerza / time) * timAct; }
 	}
 }
 
